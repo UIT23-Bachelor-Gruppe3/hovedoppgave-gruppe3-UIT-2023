851928d0b1971393ef1106cb8c032446aa2de71e
Mikal95 <mikal95@live.no>
2023-02-25 04:27:54 +0100

    Completed 5.1, 7.1 and Enemy (8.1) now follows player, but also enemies

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Transform target;
+    float speed = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!target)
+            target = collision.transform;
+    }
+}
