c576b07540da86283b1520286374a1bdad53ab16
Tore Bjerkan <torebjerkan@gmail.com>
2023-03-04 21:53:47 +0100

    cut: samplesveneold, fix: colors are applied correctly, enemy follows closest player

--- Assets/PlayerSettings.cs
+++ Assets/PlayerSettings.cs
@@ -9,7 +9,7 @@ public class PlayerSettings : NetworkBehaviour
 {
     [SerializeField] private SpriteRenderer spriteRenderer;
     [SerializeField] private TextMeshProUGUI playerName;
-    private NetworkVariable<FixedString128Bytes> networkPlayerName = new NetworkVariable<FixedString128Bytes>("Player: 0", NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server );
+    private readonly NetworkVariable<FixedString128Bytes> networkPlayerName = new("Player: 0", NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server );
 
     public List<Color> colors = new();
 
@@ -22,24 +22,7 @@ public class PlayerSettings : NetworkBehaviour
     {
         networkPlayerName.Value = "Player: " + (OwnerClientId + 1);
         playerName.text = networkPlayerName.Value.ToString();
-        //spriteRenderer.color = colors[(int)OwnerClientId];
-        switch ((int)OwnerClientId)
-        {
-            case 1:
-                Debug.Log("Black");
-                spriteRenderer.color = Color.black;
-            break;
-
-            case 2:
-                Debug.Log("Green");
-                spriteRenderer.color = Color.green;
-            break;
-
-            case 3:
-                Debug.Log("Blue");
-                spriteRenderer.color = Color.blue;
-            break;
-        }
+        spriteRenderer.color = colors[(int)OwnerClientId];
     }
 
 }
