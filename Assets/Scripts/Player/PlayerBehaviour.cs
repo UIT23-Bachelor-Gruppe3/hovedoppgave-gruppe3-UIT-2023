92e16bf9391ca415dc010e4e854da1f93ab3b4e5
Tore Bjerkan <torebjerkan@gmail.com>
2023-03-10 14:09:11 +0100

    Add: camera follows the player

--- Assets/Scripts/Player/PlayerBehaviour.cs
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -6,26 +6,47 @@ using Variables;
 
 public class PlayerBehaviour : NetworkBehaviour
 {
-    [SerializeField] FloatVariable speed;
-    public FloatReference playerSpeed;
+    [SerializeField] private FloatVariable speed;
+    [SerializeField] private FloatReference playerSpeed;
+    private Camera mainCamera;
     private float horizontalInput;
     private float verticalInput;
-    // Start is called before the first frame update
+
     void Start()
     {
         speed.SetValue(playerSpeed);
     }
 
-    // Update is called once per frame
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        //if (IsLocalPlayer == true)
+        //    playerCamera.SetActive(true);
+        //else
+        //    playerCamera.SetActive(false);
+    }
+
+    private void LateUpdate()
+    {
+        if(IsLocalPlayer)
+        {
+            Vector3 pos = transform.position;
+            pos.z = -10;
+            mainCamera.transform.position = pos;
+        }
+    }
+
     void Update()
     {
         if (!IsOwner) return;
+       
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
-    }
 
-    private void FixedUpdate()
-    {
         Vector2 movement = new Vector2(horizontalInput, verticalInput) * (playerSpeed * Time.fixedDeltaTime);
         transform.position += (Vector3)movement;
 
